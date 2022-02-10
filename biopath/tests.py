38b4e2b841b167267cca90e988f02dcd15d6cede
Mateo Martinez <mateo2000mtz@live.com>
2022-02-10 14:37:38 -0800

    Fixed backend connection

--- biopath/tests.py
+++ biopath/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+
+class SimpleTest(TestCase):
+      def setUp(self):
+            self.test_client = Client()
+
+      def test_response(self):
+            response = self.test_client.get('/biopath/')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.content, b"Hello world from django backend")
+
--- seniorProj/settings.py
+++ seniorProj/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'seniorProj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'biochemDB',
+        'USER': 'cssd07',
+        'PASSWORD': 'B10Te@m!',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
