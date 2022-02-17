d8c052040c376867e9e6d70ac7d022f5c16bed9d
Mateo Martinez <mateo2000mtz@live.com>
2022-02-16 20:12:07 -0800

    created serializers and views for serializers

---
+++ biopath/serializers.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from rest_framework_json_api import serializers
+from biopath.models import Pathway, Module, Products, Substrates
+
+class PathwaySerializer(serializers.HyperlinkedModelSerializer):
+      class Meta:
+            model = Pathway
+            fields = ('modelName')
+
+class ModuleSerializer(serializers.HyperlinkedModelSerializer):
+      class Meta:
+            model = Module
+            fields = ('enzyme', 'enzymeAbbr', 'reversible', 'xCoor', 'yCoor')
+
+class ProductsSerializer(serializers.HyperlinkedModelSerializer):
+      class Meta:
+            model = Products
+            fields = ('product', 'abbr')
+
+class SubstratesSerializer(serializers.HyperlinkedModelSerializer):
+      class Meta:
+            model = Substrates
+            fields = ('substrate', 'abbr')
--- biopath/views.py
+++ biopath/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from biopath.models import Pathway, Module, Products, Substrates
+from biopath.serializers import PathwaySerializer, ModuleSerializer, ProductsSerializer, SubstratesSerializer
+from rest_framework import viewsets
 
 # Create your views here.
 def index(request):
       return HttpResponse("Hello world from django backend")
+
+#Views that handle the HTTP requests for models
+class PathWayViewSet(viewsets.ModelViewset):
+      queryset = Pathway.objects.all()
+      serializer_class = PathwaySerializer
+
+class ModuleViewSet(viewsets.ModelViewset):
+      queryset = Module.objects.all()
+      serializer_class = ModuleSerializer
+
+class ProductsViewSet(viewsets.ModelViewset):
+      queryset = Products.objects.all()
+      serializer_class = ProductsSerializer
+
+class SubstratesViewSet(viewsets.ModelViewset):
+      queryset = Substrates.objects.all()
+      serializer_class = SubstratesSerializer
+
       
